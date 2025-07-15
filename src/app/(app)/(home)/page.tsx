db5ecf15925a59938ad330b398e5b137d42506ba
Evans-Munatsa <evansmunatsa7@gmail.com>
2025-07-15 15:52:36 +0200

    trcp first edition

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,8 +1,16 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trcp/client";
+
 export default function Home() {
+  const trpc = useTRPC();
+  const categories = useQuery(trpc.categories.getMany.queryOptions())
 
   return (
     <div>
-      Home Page
+      <p> is loading: {categories.isLoading}</p>
+      {JSON.stringify(categories.data, null, 2)}
     </div>
   );
 };
